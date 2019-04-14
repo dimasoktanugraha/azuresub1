369fb9d6c9aed02b7ca913b12c2f0f5a87aaea90
dimas_oktanugraha <d.oktanugraha@gmail.com>
2019-04-14 16:59:26 +0700

    add database

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dimassubwebappserver.database.windows.net";
+    $user = "dimas";
+    $pass = "D1m45okt4";
+    $db = "dimasSubDb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
