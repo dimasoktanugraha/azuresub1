cb4ba34cb7e501d1ed0fa1b0e82c83dd8e923039
dimas_oktanugraha <d.oktanugraha@gmail.com>
2019-04-28 16:19:11 +0700

    new4

--- vision.php
+++ vision.php
@@ -6,7 +6,7 @@
 <html>
 <head>
     <title>Analyze Image</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Bootstrap core CSS -->
     <!-- <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 </head>
